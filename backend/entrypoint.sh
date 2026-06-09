28b2699b0b939aa78fb143fa9c70b74374402913
jerryx0222 <jerryx0222@gmail.com>
2026-06-09 12:30:06 +0800

    new

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-python manage.py migrate --noinput
+#python manage.py migrate --noinput
 
 echo "Starting server..."
 exec gunicorn config.wsgi:application --bind :${PORT:-8080} --workers 1 --threads 8 --timeout 0
