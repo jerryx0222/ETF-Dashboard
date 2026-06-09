164f84a01e84636c35ff61b38dd4771247078dcd
jerryx0222 <jerryx0222@gmail.com>
2026-06-09 12:48:00 +0800

    new

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -120,3 +120,8 @@ CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS',
     'http://localhost:3000 http://127.0.0.1:3000'
 ).split()
+
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    'CSRF_TRUSTED_ORIGINS',
+    'http://localhost http://127.0.0.1'
+).split()
